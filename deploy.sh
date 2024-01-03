5b1e8227fc9538791c5dd5c53f01a765f48d7528
kariyaniamit <72973799+kariyaniamit@users.noreply.github.com>
2024-01-03 18:20:51 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Starting to deploy docker image.."
-DOCKER_IMAGE_URI = 917411321124.dkr.ecr.ap-south-1.amazonaws.com/node-app-repo:latest
-docker pull $DOCKER_IMAGE_URI
+#DOCKER_IMAGE_URI = 
+docker pull 917411321124.dkr.ecr.ap-south-1.amazonaws.com/node-app-repo:latest
 #docker ps -q --filter ancestor=$DOCKER_IMAGE_URI | xargs -r docker stop
 #docker run -d -p 5000:5000 $DOCKER_IMAGE_URI
 docker compose up -d 
